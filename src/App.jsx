74d4c2c8b1034e16d5b1a3ac2d363614eac715ac
RaziaAkbari <akbariharis14@gmail.com>
2023-10-22 17:00:37 +0430

    model positioned

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Experience from "./components/Experience"
 function App() {
   return (
     <>
-      <Canvas shadows camera={{ position: [-2, 0, 6], fov: 30 }}>
+      <Canvas shadows camera={{ position: [-2, 0, 6], fov: 20 }}>
         <Experience />
       </Canvas>
     </>
