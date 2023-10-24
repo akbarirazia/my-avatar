f2c0bbebde4a12bdecd93583877df2f331135a0c
RaziaAkbari <akbariharis14@gmail.com>
2023-10-24 13:38:03 +0430

    a spinner was set

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,12 @@ function App() {
   return (
     <>
       {loading ? (
-        <div>Loading...</div>
+        <div className="flex items-center justify-center h-screen">
+          <div className="relative">
+            <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+            <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-red-500 animate-spin"></div>
+          </div>
+        </div>
       ) : (
         <Canvas shadows camera={{ position: [-2, 0, 6], fov: 20 }}>
           <Experience />
